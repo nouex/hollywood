970ac961c5f3c4d55b0f380060925a793e246ae1
Reece Hudson <reecehudson10@gmail.com>
2018-01-10 13:56:12 -0700

    extract <ul> to own component <ChatList />

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ChatList from './ChatList';
 
 class ChatWindow extends Component {
 
@@ -13,11 +14,7 @@ class ChatWindow extends Component {
     return (
       <div>
         <h3>Stukent Chat App</h3>
-        <ul>
-          {this.state.items.map(item => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-        </ul>
+        <ChatList items={this.state.items}/>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} value={this.state.text} />
           <button>{'Send'}</button>
