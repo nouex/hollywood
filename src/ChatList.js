ec113d41e80c735f1618f267a05461e3b924c6a8
Reece Hudson <reecehudson10@gmail.com>
2018-01-10 14:18:52 -0700

    display time next to msg

--- src/ChatList.js
+++ src/ChatList.js
@@ -3,9 +3,26 @@ import React from 'react';
 function ChatList({items}) {
   return (
     <ul>
-      {items.map(item => (
-        <li key={item.id}>{item.text}</li>
-      ))}
+      {items.map(({id: date, text}) => {
+        let item = <li className="d-inline">{text}</li>,
+            formattedTime
+
+        let hrs, mins, secs, meridiem,
+            time = new Date(date)
+        hrs = time.getHours() % 12
+        meridiem = hrs > 12 ? "AM" : "PM"
+        hrs = hrs === 0 ? 12 : hrs
+        mins = time.getMinutes()
+        secs = time.getSeconds()
+        formattedTime = [hrs, mins, secs].join(":") + ` ${meridiem}: `
+
+        return (
+          <div key={date}>
+            {formattedTime}
+            "{item}"
+          </div>
+        )
+      })}
     </ul>
   )
 }
