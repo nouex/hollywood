73ef6cbacd664d43ec9c1718b77f889f9ce54ded
Reece Hudson <reecehudson10@gmail.com>
2018-01-10 14:51:27 -0700

    use 'fromnow' timestamp

--- src/ChatList.js
+++ src/ChatList.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import moment from 'moment';
 
-function ChatList({items}) {
-  return (
-    <ul>
-      {items.map(({id: date, text}) => {
-        let item = <li className="d-inline">{text}</li>,
-            formattedTime
+class ChatList extends Component {
+  constructor(props) {
+    super(props)
+    this.timeoutId = null
+  }
 
-        let hrs, mins, secs, meridiem,
-            time = new Date(date)
-        hrs = time.getHours() % 12
-        meridiem = hrs > 12 ? "AM" : "PM"
-        hrs = hrs === 0 ? 12 : hrs
-        mins = time.getMinutes()
-        secs = time.getSeconds()
-        formattedTime = [hrs, mins, secs].join(":") + ` ${meridiem}: `
+  componentWillMount() {
+    this.updateTime()
+  }
 
-        return (
-          <div key={date}>
-            {formattedTime}
-            "{item}"
-          </div>
-        )
-      })}
-    </ul>
-  )
+  componentWillUnMount() {
+    if (null !== this.timeoutId) {
+      clearTimeout(this.timeoutId)
+    }
+  }
+
+  updateTime() {
+    this.forceUpdate()
+    this.timeoutId = setTimeout(this.updateTime.bind(this), 3000)
+  }
+
+  render() {
+    let {items} = this.props
+    return (
+      <ul>
+        {items.map(({id: date, text}) => {
+          let item = <li className="d-inline">{text}</li>,
+              formattedTime = moment(date).fromNow() + ": "
+
+          return (
+            <div key={date}>
+              {formattedTime}
+              "{item}"
+            </div>
+          )
+        })}
+      </ul>
+    )
+  }
 }
 
 export default ChatList
